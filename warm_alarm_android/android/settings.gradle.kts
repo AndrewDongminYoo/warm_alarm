c5f578512bd9e18aa563ce7436725002aff33cd5
Dongmin, Yu <ydm2790@gmail.com>
2026-08-30 08:59:55 +0900

    build(android): align Kotlin plugin for native tests

--- warm_alarm_android/android/settings.gradle.kts
+++ warm_alarm_android/android/settings.gradle.kts
@@ -10,5 +10,5 @@ rootProject.name = "warm_alarm_android"
 
 plugins {
     id("com.android.library") version "8.12.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.10" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
 }
